56960e8e295a3408b63a1ed832ebd76401639497
webnetkz <8gg001@gmail.com>
2019-12-21 17:27:14 +0600

    append service worker

--- public/tamplates/footer.php
+++ public/tamplates/footer.php
@@ -1,5 +1,5 @@
     <footer>
-        <p style="margin-top: 60%; color: white;">2019/12/21</p>
+        <p style="margin-top: 60%; color: blue;">2019/12/21</p>
     </footer>
 
     <script src="public/scripts/libs.js"></script>
